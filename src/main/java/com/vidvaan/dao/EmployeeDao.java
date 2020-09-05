5a310ad8c59545284bb23a43397bae3720a50daf
Microsoft <Microsoft@Microsoft-PC>
2020-09-05 23:12:07 +0530

    WH-8582-FIRST TIME FILE UPDATE

---
+++ src/main/java/com/vidvaan/dao/EmployeeDao.java
@@ -0,0 +1,16 @@
+package com.vidvaan.dao;
+
+import com.vidvaan.model.Employee;
+
+public interface EmployeeDao {
+	public abstract void createEmployee(Employee employee);
+
+	public abstract void updateEmployeeEmailById(String newEmail ,int eid);
+
+	public abstract void deleteEmployeeById(int eid);
+
+	public abstract Employee getEmployeeById(int eid);
+
+	public abstract void  getAllEmplyeesDetails();  //Returns List of Employees
+
+}
---
+++ src/main/java/com/vidvaan/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.vidvaan.service;
+
+import com.vidvaan.model.Employee;
+
+public interface EmployeeService {
+	public abstract void addEmployee(Employee employee);
+
+	public abstract void updateEmployeeEmailById(String newEmail ,int eid);
+
+	public abstract void deleteEmployeeById(int eid);
+
+	public abstract Employee fetchEmployeeById(int eid);
+
+	public abstract void getAllEmplyeesInfo();
+
+
+}
